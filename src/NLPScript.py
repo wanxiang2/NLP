c3a39f6cfc308b45588dcbc5e9904dc2a4daeda5
wanxiang2 <wanxiang@buffalo.edu>
2024-06-25 16:56:56 -0400

    Added additional Comments.

--- src/NLPScript.py
+++ src/NLPScript.py
@@ -10,13 +10,18 @@ from bs4 import BeautifulSoup
 # Google Patents API Documentation:
 # https://serpapi.com/google-patents-api
 #
+# Google Patents Details API Documentation:
+# https://serpapi.com/google-patents-details-api
+#
 # This program will save all patents about food packaging products from the US Patents Office in its own folder that 
 # will be named after its Patent ID using Google Patents API. All these patent directories will be downloaded to the 
 # current directory in your computer. Each patent folder will contain a JSON file (which holds JSON data on the patent 
 # such as title, id, URL to the PDF, and URLs to figures), a text file that contains all the text from the patent 
-# document, and all patent images belonging to that patent (if any).
+# document, and all patent images belonging to that patent (if any). The program will get the text files by using the
+# Google Patents Details API, which will be queried for each patent that you need to extract text to a .txt file for.
 
 def main():
+    # Sets up command line arguments and flag options.
     parser = argparse.ArgumentParser(description="Contains the query and any flags to specify if you only want the log file, text, or figures.")
     parser.add_argument('query', help='You must pass an argument for what query you would like to make.')        
     
@@ -119,6 +124,10 @@ def print_page(page_number, my_query, my_flags):
 
             if '-t' in my_flags:
                 print("\nWriting text to " + patent_directory + ".txt text file...")
+
+                # Uses the Google Patents Details API and the Patent IDs received from the Google Patents API to 
+                # get the full text of each patent in the form of an html webpage. Then Beautiful Soup is used
+                # to convert it to writeable text.
                 details_url = "https://serpapi.com/search?engine=google_patents_details"
                 details_params = {
                     "engine": "google_patents_details",
@@ -135,33 +144,17 @@ def print_page(page_number, my_query, my_flags):
                     html.encoding = 'utf-8'
                     html_text = html.text
 
-                    #html_file = open(patent_directory + '_html.txt', 'w')
-                    #html_file.write(html_text)
-                    #html_file.close()
-
                     soup = BeautifulSoup(html_text, 'html.parser')
-
-                    #for img in soup.find_all('img'):
-                    #    img.decompose()
 
                     target_paragraphs = soup.find_all(attrs={"class": "description-paragraph"})
                     target_text = [paragraph.get_text() for paragraph in target_paragraphs]
-
-                    #text = target_elements.get_text(separator='\n', strip=True) 
                     
-#Bridget said to use .splitlines() on target_text which should put all the lines in an array. Then loop through that array and write to .txt file. She said to still keep the repr(). Then we can take out any weird stuff using regex.
+                    # Write the text to a .txt file. Writes a line space for each paragraph in the .txt.
                     with open(patent_directory + '.txt', 'w', encoding='utf-8') as file:
                         for text in target_text:
                             for line in text.splitlines():
-                                file.write(line)        #There's also option to remove the repr(). Then there won't be the \n character and instead will actually go to the next line in the text.
-                                                        #The '' will also disappear. However, this wierd box will appear wherever there is an image. Since we're doing NLP, I feel like the ''
-                            file.write("\n\n")          #and the \n character will be fine. But ask Dr. Bridget what she thinks! You've saved the .txt file (the _LOOK file) from the execution with the repr() removed.
-
-
-
-                    #text_file = open(patent_directory + '.txt', 'w')
-                    #text_file.write(target_text)
-                    #text_file.close()
+                                file.write(line)        
+                            file.write("\n\n")          
 
                     print("\nDone.\n")
 
@@ -186,9 +179,6 @@ def print_page(page_number, my_query, my_flags):
 
 if __name__ == "__main__":
     main()
-
-
-
 
 
 
